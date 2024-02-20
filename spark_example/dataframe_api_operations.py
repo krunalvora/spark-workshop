934577f436b29db8e057e8ce35739767efb578c7
Krunal Vora <krunalvora4@gmail.com>
2024-02-19 16:11:50 -0800

    Update dataframe_api_operations.py

--- spark_example/dataframe_api_operations.py
+++ spark_example/dataframe_api_operations.py
@@ -1,16 +1,23 @@
 from pyspark.sql import SparkSession
 
-
 spark = (SparkSession
-    .builder
-    .getOrCreate()) 
+         .builder
+         .getOrCreate())
 
-ev_data_csv_path = "datasets/ev_population_data.csv"
+ev_data_csv_path = "../datasets/ev_population_data.csv"
 
+# Read from CSV file
 ev_df = spark.read.csv(ev_data_csv_path, header=True, samplingRatio=0.001)
+print(ev_df.dtypes)
 
 ev_df.show()
 
+ev_df = ev_df.filter(ev_df['Electric Range'] >= 337)
+
+ev_df.show()
+print(f"Count of EVs with range >= 337: {ev_df.count()}")
+
+# Write to CSV file
+ev_df.write.csv("../tmp/test.csv", mode="overwrite")
+
 spark.stop()
-
-
